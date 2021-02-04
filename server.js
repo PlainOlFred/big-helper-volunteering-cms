26109386ea80c34d92b949fb4167a06cfef51847
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2021-02-03 21:43:32 -0600

    Created react app to deploy to heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require('path');
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(require('./controllers'));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
+
